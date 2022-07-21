e440f0eb9bef98f2f4ccbfd7b3daa89d36e138b9
Nishanov Khasan <99598107+HasanNishanov@users.noreply.github.com>
2022-07-21 14:24:00 +0500

    Ba-da-bum-tsssssss

--- src/main/java/school/mjc/stage0/conditions/task3/DaysInMonthPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task3/DaysInMonthPrinter.java
@@ -2,5 +2,22 @@ package school.mjc.stage0.conditions.task3;
 
 public class DaysInMonthPrinter {
     public void amountOfDays(int month) {
+        if(month ==1 || month==3 || month==4 || month==7|| month==9|| month==10 ){
+            System.out.println("31");
+        }
+        if (month==2){
+            System.out.println("28");
+        }
+        if(month==4 || month==6 || month==9 || month==117){
+            System.out.println("30");
+        }
+        else if (month >12 || month<1){
+            System.out.println("wrong number!");
+        }
+    }
+
+    public static void main(String[] args) {
+        DaysInMonthPrinter daysInMonthPrinter = new DaysInMonthPrinter();
+        daysInMonthPrinter.amountOfDays(2);
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -1,6 +1,32 @@
 package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
-    public void calculateSalary(int salary) {
+    public void calculateSalary(double salary) {
+        double salary2 = (double) salary;
+        if(salary > 1 && salary <= 10000){
+            double i = salary *= 0.15;
+            salary2 -= i;
+            System.out.println(salary2);
+        }
+        else if(salary > 10000 && salary <= 20000){
+            // 18%
+            double i = salary *= 0.18;
+            salary2 -= i;
+            System.out.println(salary2);
+        }
+        else if(salary > 20000 ){
+            // 20%
+            double i = salary *= 0.20;
+            salary2 -= i;
+            System.out.println(salary2);
+        }
+        else if(salary <= 0 ){
+            System.out.println("wrong input!");
+        }
+    }
+
+    public static void main(String[] args) {
+        SalaryCalculator salaryCalculator = new SalaryCalculator();
+        salaryCalculator.calculateSalary(-1);
     }
 }
